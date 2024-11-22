f81181e337ab5f2c99936f728ec5284eb6c5e8f3
YiyangGao <daniel-yiyang.gao@cn.abb.com>
2024-11-22 11:30:59 +0800

    添加Docker容器

---
+++ Login/Controllers/ContentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    [Authorize] // 确保页面受保护
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userName = User.Identity?.Name; // 获取当前登录用户
+            return View(model: userName);
+        }
+    }
+}
