2277dea84b816e70c6676af397bbb85e8c7a727e
YiyangGao <daniel-yiyang.gao@cn.abb.com>
2024-11-25 10:11:14 +0800

    两个应用成功接入Keycloak

---
+++ Logon2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Logon2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
